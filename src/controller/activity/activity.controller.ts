eade6c0ed590cd94a5ce20676c62f90963850885
firay24 <fisan.onodaconan@gmail.com>
2025-01-29 21:36:34 +0700

    build: update dependencies and add xlsx
    - Updated several dependencies to their latest versions.
    - Added xlsx package for Excel file processing.
    - Updated @types packages for express and multer.

--- src/controller/activity/activity.controller.ts
+++ src/controller/activity/activity.controller.ts
@@ -6,6 +6,10 @@ import { PrismaService } from 'src/services/prisma.service';
 import { UtilityService } from 'src/services/utility.service';
 import { CompetitionSaveDto } from 'src/types/controller/competition/competition.dto';
 import { Request } from 'express';
+// import * as fs from 'fs';
+// import * as path from 'path';
+// import { diskStorage } from 'multer';
+// import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 @UseGuards(RolesGuard)
@@ -109,5 +113,36 @@ export class ActivityController {
       data: { id: competition.Id },
     });
   }
+  // #endregion
+
+  // #region batch save
+  // @Post('batch-save')
+  // @Roles([Role.SUPERADMIN, Role.ADMIN, Role.EVENTADMIN, Role.FACILITATOR])
+  // @UseInterceptors(
+  //   FileInterceptor('file', {
+  //     storage: diskStorage({
+  //       destination: './uploads',
+  //       filename: (req, file, cb) => {
+  //         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+  //         cb(null, uniqueSuffix + path.extname(file.originalname));
+  //       },
+  //     }),
+  //     fileFilter: (req, file, cb) => {
+  //       if (!file.originalname.match(/\.(xlsx|xls)$/)) {
+  //         return cb(new BadRequestException('Only Excel files are allowed'), false);
+  //       }
+  //       cb(null, true);
+  //     },
+  //   }),
+  // )
+  // async uploadFile(@UploadedFile() file: Express.Multer.File, @Body('schoolId') schoolId: string, @Body('competitionId') competitionId: string) {
+  //   if (!file) {
+  //     throw new BadRequestException('No file uploaded');
+  //   }
+
+  //   return {
+  //     message: 'Upload sukses',
+  //   };
+  // }
   // #endregion
 }
