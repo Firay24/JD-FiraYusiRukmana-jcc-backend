f40235ba66ca3d903d73ce63335b560b36abeeda
firay24 <fisan.onodaconan@gmail.com>
2025-01-29 23:16:26 +0700

    feat(event): add event search endpoint
    - Added new endpoint to search for events
    - Implemented search functionality by stage, level, and subject ID
    - Included error handling for non-existent users and events
    - Returns event ID upon successful search

--- src/controller/event/event.controller.ts
+++ src/controller/event/event.controller.ts
@@ -6,6 +6,7 @@ import { PrismaService } from 'src/services/prisma.service';
 import { UtilityService } from 'src/services/utility.service';
 import { EventSaveDto } from 'src/types/controller/event/event.dto';
 import { Request } from 'express';
+import { StageType } from '@prisma/client';
 
 @Controller()
 @UseGuards(RolesGuard)
@@ -14,6 +15,48 @@ export class EventController {
     private prismaService: PrismaService,
     private utilityService: UtilityService,
   ) {}
+
+  // #region search
+  @Get('search')
+  @Roles([Role.SUPERADMIN, Role.ADMIN, Role.EVENTADMIN, Role.PARTISIPANT, Role.FACILITATOR])
+  async search(@Req() request: Request, @Query('stage') stage: string, @Query('level') level: string, @Query('subjectId') subjectId: string) {
+    const user = request.user;
+    const dbUser = await this.prismaService.user.findFirst({
+      where: { Id: user.id },
+      include: { Role: true },
+    });
+
+    if (!dbUser) {
+      throw new BadRequestException(
+        this.utilityService.globalResponse({
+          statusCode: 400,
+          message: 'User not found',
+        }),
+      );
+    }
+
+    const dbEvent = await this.prismaService.competition.findFirst({
+      where: { Stage: stage as StageType, Level: parseInt(level, 10), SubjectId: subjectId },
+    });
+
+    if (!dbEvent) {
+      throw new BadRequestException(
+        this.utilityService.globalResponse({
+          statusCode: 400,
+          message: 'Event not found',
+        }),
+      );
+    }
+
+    return this.utilityService.globalResponse({
+      statusCode: 200,
+      message: 'Success',
+      data: {
+        id: dbEvent ? dbEvent.Id : '',
+      },
+    });
+  }
+  // #endregion
 
   // #region list
   @Get('list')
