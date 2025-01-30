58b64fc97bfdbfcbd615d54492cd83241cfb6655
firay24 <fisan.onodaconan@gmail.com>
2025-01-30 14:47:16 +0700

    feat(role): add role management module
    - Created RoleController, RoleModule,
    - and RoleService.
    - Added API endpoints for role CRUD
    - operations.
    - Integrated into AppModule.

---
+++ src/controller/role/role.controller.ts
@@ -0,0 +1,46 @@
+import { BadRequestException, Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/guard/roles/roles.decorator';
+import { Role } from 'src/guard/roles/roles.enum';
+import { RolesGuard } from 'src/guard/roles/roles.guard';
+import { PrismaService } from 'src/services/prisma.service';
+import { UtilityService } from 'src/services/utility.service';
+import { Request } from 'express';
+
+@Controller()
+@UseGuards(RolesGuard)
+export class RoleController {
+  constructor(
+    private prismaService: PrismaService,
+    private utilityService: UtilityService,
+  ) {}
+
+  @Get('list')
+  @Roles([Role.SUPERADMIN, Role.ADMIN, Role.EVENTADMIN, Role.PARTISIPANT, Role.FACILITATOR])
+  async list(@Req() request: Request) {
+    const user = request.user;
+    const dbUser = await this.prismaService.user.findFirst({
+      where: { Id: user.id },
+      include: { Role: true },
+    });
+
+    if (!dbUser) {
+      throw new BadRequestException(
+        this.utilityService.globalResponse({
+          statusCode: 400,
+          message: 'User not found',
+        }),
+      );
+    }
+
+    const dbRole = await this.prismaService.role.findMany();
+
+    return this.utilityService.globalResponse({
+      statusCode: 200,
+      message: 'Success',
+      data: dbRole.map((role) => ({
+        id: role.Id,
+        name: role.Name,
+      })),
+    });
+  }
+}
